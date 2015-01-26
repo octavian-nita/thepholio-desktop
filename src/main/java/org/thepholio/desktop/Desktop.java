8bfb4ccb883c0e7468d8c0a97e344056c8493275
Octavian Nita <mnothro@yahoo.com>
2015-01-26 16:54:45 +0100

    using the TwelveMonkeys utility libraries to read more image formats

--- src/main/java/org/thepholio/desktop/Desktop.java
+++ src/main/java/org/thepholio/desktop/Desktop.java
@@ -1,6 +1,7 @@
 package org.thepholio.desktop;
 
 import javafx.application.Application;
+import javafx.embed.swing.SwingFXUtils;
 import javafx.geometry.HPos;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
@@ -8,16 +9,19 @@ import javafx.geometry.Rectangle2D;
 import javafx.scene.Scene;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.ScrollPane;
+import javafx.scene.control.SelectionModel;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.StackPane;
 import javafx.scene.text.Text;
-import javafx.scene.text.TextAlignment;
 import javafx.stage.Screen;
 import javafx.stage.Stage;
 
+import javax.imageio.ImageIO;
 import java.io.File;
+import java.io.IOException;
+import java.net.URL;
 
 import static java.lang.Double.MAX_VALUE;
 import static javafx.scene.layout.Priority.ALWAYS;
@@ -25,46 +29,51 @@ import static org.thepholio.desktop.Config.SAMPLES;
 
 public class Desktop extends Application {
 
-    @Override
-    public void start(Stage primaryStage) throws Exception {
+    private ImageView imageView = new ImageView();
 
-        ImageView imageView = new ImageView();
+    private ComboBox samplesCB = new ComboBox();
+
+    private Text statusBar = new Text();
+
+    public Desktop() {
         imageView.setPreserveRatio(true);
         imageView.setSmooth(true);
         imageView.setCache(true);
+
+        samplesCB.setItems(SAMPLES);
+        samplesCB.setMaxWidth(MAX_VALUE);
+        samplesCB.setOnAction(event -> displaySelectedImage());
+
+        statusBar.setId("status");
+    }
+
+    private void displaySelectedImage() {
+        @SuppressWarnings("unchecked") SelectionModel<File> selectionModel = samplesCB.getSelectionModel();
+        if (!selectionModel.isEmpty()) {
+            try {
+                imageView.setImage(getImage((selectionModel.getSelectedItem()).toURI().toURL()));
+            } catch (Exception e) {
+                statusBar.setText(e.getMessage());
+            }
+        }
+    }
+
+    private Image getImage(URL url) throws IOException {
+        return SwingFXUtils.toFXImage(ImageIO.read(url), null);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
 
         ScrollPane scroll = new ScrollPane() {
 
             public void requestFocus() {}
         };
         scroll.setPannable(true);
-        scroll.setContent(new StackPane(imageView)); // allows content aligning and also centers it by default!
         scroll.setFitToWidth(true);
+        scroll.setContent(new StackPane(imageView)); // allows content aligning and also centers it by default!
 
-        Text statusBar = new Text("Found " + SAMPLES.size() + " samples.");
-        statusBar.setId("status");
-        statusBar.setTextAlignment(TextAlignment.RIGHT);
-
-        ComboBox samplesCB = new ComboBox(SAMPLES);
-        samplesCB.setMaxWidth(MAX_VALUE);
-        if (SAMPLES.size() > 0) {
-            samplesCB.getSelectionModel().select(0);
-            try {
-                imageView.setImage(new Image(
-                    ((File) samplesCB.getSelectionModel().getSelectedItem()).toURI().toURL().toExternalForm()));
-            } catch (Exception e) {
-                statusBar.setText(e.getMessage());
-            }
-        }
-
-        samplesCB.setOnAction(event -> {
-            try {
-                imageView.setImage(new Image(
-                    ((File) samplesCB.getSelectionModel().getSelectedItem()).toURI().toURL().toExternalForm()));
-            } catch (Exception e) {
-                statusBar.setText(e.getMessage());
-            }
-        });
+        statusBar.setText("Found " + SAMPLES.size() + " samples.");
 
         GridPane root = new GridPane();
         root.setPadding(new Insets(4));
@@ -85,12 +94,14 @@ public class Desktop extends Application {
         GridPane.setHalignment(statusBar, HPos.RIGHT);
 
         Rectangle2D screenBounds = Screen.getPrimary().getVisualBounds();
-        Scene scene = new Scene(root, screenBounds.getWidth() * 4 / 5, screenBounds.getHeight() * 4 / 5);
+        Scene scene = new Scene(root, screenBounds.getWidth() * 3 / 4, screenBounds.getHeight() * 3 / 4);
         scene.getStylesheets().add(getClass().getResource("/themes/default/main.css").toExternalForm());
 
         primaryStage.setTitle("ThePholio Desktop");
         primaryStage.setScene(scene);
         primaryStage.show();
+
+        samplesCB.getSelectionModel().select(0);
     }
 
     public static void main(String[] args) {
